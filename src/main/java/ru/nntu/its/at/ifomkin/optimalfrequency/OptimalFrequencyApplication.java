4fb4887df0210c817188a0ba5d2ef239c91cfe4a
Ivan Fomkin <ivan-fomkin@yandex.ru>
2023-11-19 18:16:57 +0300

    Initial commit

---
+++ src/main/java/ru/nntu/its/at/ifomkin/optimalfrequency/OptimalFrequencyApplication.java
@@ -0,0 +1,13 @@
+package ru.nntu.its.at.ifomkin.optimalfrequency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalFrequencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimalFrequencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nntu/its/at/ifomkin/optimalfrequency/OptimalFrequencyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nntu.its.at.ifomkin.optimalfrequency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalFrequencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
