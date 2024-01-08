3b9cd858e250b403257bee58cac992f411db5320
Ivan Fomkin <ivan-fomkin@yandex.ru>
2024-01-08 12:45:51 +0300

    Добавил favicon.ico, исправил отображение ошибок, исправил заголовок приложения

--- src/main/java/ru/nntu/its/at/ifomkin/optimalfrequency/controller/ErrorController.java
+++ src/main/java/ru/nntu/its/at/ifomkin/optimalfrequency/controller/ErrorController.java
@@ -1,14 +1,18 @@
 package ru.nntu.its.at.ifomkin.optimalfrequency.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.nntu.its.at.ifomkin.optimalfrequency.model.OptimalMileageRequest;
+import ru.nntu.its.at.ifomkin.optimalfrequency.util.AcceptableLevelsOfFailureFreeOperationUtil;
 
 @ControllerAdvice
 public class ErrorController {
     @ExceptionHandler(Exception.class)
-    public String errorMessage(Exception exception, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("errorMessage", exception.getMessage());
-        return "redirect:/";
+    public String errorMessage(Exception exception, Model model) {
+        model.addAttribute("acceptableLevels", AcceptableLevelsOfFailureFreeOperationUtil.ACCEPTABLE_VALUES);
+        model.addAttribute("errorMessage", exception.getMessage());
+        model.addAttribute("request", new OptimalMileageRequest());
+        return "index";
     }
 }
